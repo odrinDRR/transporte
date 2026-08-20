79d9ca779d99ae5a9dabdbaed0528e49afce488e
orizquez <odrinrizquez@gmail.com>
2026-08-20 16:49:11 -0400

    añadido el login

--- transporte-flota/src/app/app.module.ts
+++ transporte-flota/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MantenimientoComponent } from './modules/mantenimiento/mantenimiento.co
 import { DocumentosComponent } from './modules/documentos/documentos.component';
 import { CombustibleComponent } from './modules/combustible/combustible.component';
 import { FilterStatusPipe } from './shared/pipes/filter-status.pipe';
+import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FilterStatusPipe } from './shared/pipes/filter-status.pipe';
     MantenimientoComponent,
     DocumentosComponent,
     CombustibleComponent,
-    FilterStatusPipe
+    FilterStatusPipe,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
